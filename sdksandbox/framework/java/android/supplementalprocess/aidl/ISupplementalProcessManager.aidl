990a79cbb4f44babea5af869aeda52ddeeacdd3e
Samiul Islam <samiul@google.com>
2021-11-03 20:06:53 +0000

    Add skeleton of SupplementalProcessManagerService 1/n
    Simply creates aidl file for the service and it's corresponding java
    class in appropriate directory.
    
    More skeleton code will be added on follow up CL. Uploaded this CL early
    to unblock others who will be working on other parts of the skeleton
    code.
    
    Unit tests will be added when we start filling in the the
    implmenetations.
    
    Bug: 204991850
    Test: m service-supplementalprocess
    Change-Id: I33a69accccc73277d332f2926bde97b805f7089f

---
+++ sdksandbox/framework/java/android/supplementalprocess/aidl/ISupplementalProcessManager.aidl
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.supplementalprocess.aidl;
+
+import android.os.Bundle;
+import android.os.IBinder;
+import android.supplementalprocess.aidl.IInitCodeCallback;
+
+/** @hide */
+interface ISupplementalProcessManager {
+    void loadCode(in String name, in String version, in Bundle params, in IInitCodeCallback callback);
+    void requestSurfacePackage(int id, in IBinder token, int displayId, in Bundle params);
+    void sendData(int id, in Bundle params);
+    void destroyCode(int id);
+}
---
+++ sdksandbox/service/java/com/android/server/supplementalprocess/SupplementalProcessManagerService.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.supplementalprocess;
+
+import android.annotation.NonNull;
+import android.content.Context;
+import android.os.RemoteException;
+
+import android.os.Bundle;
+import android.os.IBinder;
+
+import android.util.Log;
+
+import android.supplementalprocess.aidl.IInitCodeCallback;
+import android.supplementalprocess.aidl.ISupplementalProcessManager;
+
+import com.android.server.SystemService;
+
+
+/** @hide **/
+public class SupplementalProcessManagerService extends ISupplementalProcessManager.Stub {
+
+    private static final String TAG = "SupplementalProcessManager";
+
+    private final Context mContext;
+
+    private SupplementalProcessManagerService(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void loadCode(String name, String version, Bundle params, IInitCodeCallback callback) {}
+
+    @Override
+    public void requestSurfacePackage(int id, IBinder token, int displayId, Bundle params) {}
+
+    @Override
+    public void sendData(int id, Bundle params) {}
+
+    @Override
+    public void destroyCode(int id) {}
+
+    /** @hide */
+    public static class Lifecycle extends SystemService {
+        public Lifecycle(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onStart() {
+            SupplementalProcessManagerService service =
+                    new SupplementalProcessManagerService(getContext());
+            // publishBinderService(Context.SUPPLEMENTAL_PROCESS_SERVICE, service);
+            Log.i(TAG, "SupplementalProcessManagerService started!");
+        }
+    }
+}
